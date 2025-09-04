34710e863047b19ae1fc1cecc54b1ff4d182de2d
Axel mac <axelhernandezperez23@gmail.com>
2025-09-04 15:56:25 -0600

    adding products page mockup

---
+++ src/components/common/FilterProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FilterProducts = () => {
+  return (
+    <div>
+      <div id='filterProductss' className='border border-stone-300 p-2 cursor-pointer hover:bg-stone-200'>
+        <a href='#'>Filter</a>
+      </div>
+    </div>
+  )
+}
+
+export default FilterProducts
